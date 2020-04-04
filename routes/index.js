787c7329cdba0cb0880f1cd4942ad0a19f5035d9
Dragos Sandu <dragos.n.sandu@gmail.com>
2020-04-04 16:46:09 +0300

    Add first steps for the interface

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var ttn_content;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',  });
+});
+
+router.post('/ttn', function(req, res, next) {
+  // TODO: create parser of POST request coming from TTN
+  // TODO: validate info & save it in a database structure
+  ttn_content = req.body;
+  console.log('Got body:', req.body);
+  res.sendStatus(200);
 });
 
 module.exports = router;
